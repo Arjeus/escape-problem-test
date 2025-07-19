a8ef33aacc47753a9f3b14336caebaafecdba530
ARJEUS ALVAREZ GUEVARRA <152669910+Arjeus@users.noreply.github.com>
2025-07-20 03:27:37 +0800

    Create setup.py
    mysetup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="escape",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    author="chatgpt o3",
+    description="A simple Python project that uses numpy.",
+)
